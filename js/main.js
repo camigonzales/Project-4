ea91b4cd125a07d2b43847c0f15e358de49e3269
camigonzales <camileighgonzales@gmail.com>
2024-11-26 09:19:35 -0600

    Make live

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// JS scripts placed here
+const welcomecontainer = document.querySelector('.welcomecontainer');
+const cgdesignslogo = document.getElementById('cgdesignslogo');
+const paintedbycamilogo = document.getElementById('paintedbycamilogo');
+const paintedbycamiswipe = document.getElementById('paintedbycamiswipe');
+const cgdesignsswipe = document.getElementById('cgdesignsswipe');
+
+
+cgdesignslogo.addEventListener('click', () => {
+    welcomecontainer.style.transform = 'translateX(0)';
+    });
+    
+paintedbycamilogo.addEventListener('click', () => {
+    welcomecontainer.style.transform = 'translateX(-200vw)';
+    }); 
+
+paintedbycamiswipe.addEventListener('click', () => {
+    welcomecontainer.style.transform = 'translateX(-200vw)';
+    }); 
+
+cgdesignsswipe.addEventListener('click', () => {
+    welcomecontainer.style.transform = 'translateX(0)';
+    });
